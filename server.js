f6e09dd3ebe8c1fe090e0d637791292d3cc9d361
looper-m <mothil.vu@gmail.com>
2021-04-16 21:30:15 -0700

    Add CORS

--- server.js
+++ server.js
@@ -2,32 +2,34 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const session = require('express-session')
+// const session = require('express-session')
+
+let port = process.env.PORT || 3000
 
 mongoose.connect(
     'mongodb+srv://root:muttonroganjosh@cluster0.y0cku.mongodb.net/blueboard?retryWrites=true&w=majority',
     {useNewUrlParser: true, useUnifiedTopology: true}
     );
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    // cookie: { secure: true }
-}))
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://wbdv-sp21-mothil-client-react.herokuapp.com');
     res.header('Access-Control-Allow-Headers',
-        'Content-Type, X-Requested-With, Origin');
+        'Content-Type, Authorization, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
         'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 });
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: false,
+//     saveUninitialized: true,
+//     // cookie: { secure: true }
+// }))
 
 require('./controllers/quizzes-controller')(app)
 require('./controllers/questions-controller')(app)
 require('./controllers/quiz-attempts-controller')(app)
 
-app.listen(4000)
+app.listen(port)
